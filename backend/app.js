cf7b835c84a7cb1eeb338dbff53a0d9f7cc6d7de
Mayur <mayurborsare777@gmail.com>
2022-06-07 12:20:10 +0530

    final try

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
-
+let cors=require("cors");
 
 
 const errorMiddleware = require("./middleware/error");
